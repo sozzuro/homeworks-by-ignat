5634c96a25022e2bdc42789570b37bf448f4ae6a
Denis Radivil <sozzuro@gmail.com>
2022-03-05 15:47:41 +0300

    feat: created Message component and styling

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import styles from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={styles.message}>
+            <div className={styles.avatarWrapper}>
+                <img src={props.avatar} alt=""/>
+            </div>
+            <div className={styles.messageWrapper}>
+                <h3 className={styles.nickname}>{props.name}</h3>
+                <p className={styles.text}>{props.message}</p>
+                <span className={styles.dateMessage}>{props.time}</span>
+            </div>
         </div>
     )
 }
